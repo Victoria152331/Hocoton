8ce402154a85cf311ee340c0cb021fe8881fd5fc
KristinaFi <75157579+KristinaFi@users.noreply.github.com>
2023-03-14 12:00:22 +0900

    Create card.js

---
+++ card.js
@@ -0,0 +1,10 @@
+function ShowNone(){
+	popa = document.getElementById('bebe')
+	popa.style.display = 'block'
+
+}
+function Pu(){
+	popa = document.getElementById('bebe')
+	popa.style.display = 'none'
+	document.getElementById('popa').innerHTML = 'Принято'
+}
